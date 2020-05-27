1f234c8ebf6369f29fb813e25e8372e398896728
Jonas Schmitt <jonas.schmitt@me.com>
2020-05-27 08:58:55 +0200

    add: lib to git

---
+++ lib/index.d.ts
@@ -0,0 +1,15 @@
+import { ConfigOptions, ConfigFileOptions, RequiredConfigFileOptions } from './src/types';
+export default class Config {
+    private static instance;
+    private isSetup;
+    private configOptions;
+    private env;
+    private constructor();
+    static getInstance(): Config;
+    setup(configOptions?: ConfigOptions): ConfigOptions | undefined;
+    private loadEnv;
+    getEnv(key: string): string | undefined;
+    requireEnv(key: string): string;
+    get(configFileOptions: ConfigFileOptions): Promise<any>;
+    require(configFileOptions: RequiredConfigFileOptions): Promise<any>;
+}
---
+++ lib/src/types.d.ts
@@ -0,0 +1,13 @@
+export declare type ConfigFileOptions = {
+    filePath?: string;
+    fileName?: string;
+    key?: string;
+};
+export declare type RequiredConfigFileOptions = {
+    filePath?: string;
+    fileName?: string;
+    key: string;
+};
+export declare type ConfigOptions = {
+    configFilesPath?: string;
+};
